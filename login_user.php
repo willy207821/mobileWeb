bf24da869002d9c9bdee60d96f559d5d5c5fc734
Willy <willycaparas@gmail.com>
2021-04-19 09:37:03 +0800

    login form create

---
+++ login_user.php
@@ -0,0 +1,49 @@
+<?php
+session_start();
+$_SESSION['active_user']=null;
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Login Form</title>
+<link type="text/css "rel="stylesheet" href="css/login_design.css"/>
+</head>
+<body class='site'>
+<!--Login Form-->
+<form method='POST' action='login_process.php'>
+	<div class = 'login' >
+		<p>Please enter your credentials to login</p> 
+		<div class ='usernane'>
+		<input type='text' name='username' placeholder='User Name'/>
+		</div>
+		<div class ='password'>
+ 		<input type='password' name='password'placeholder='Password'>
+ 		</div>
+ 		<div class ='button'>
+   		<input type='submit' name='submit' value='Login'>
+   		</div>
+	</div>
+</form>
+<?php
+if(!isset($_GET['signup']))
+{
+	exit();
+}else
+{
+	$check_signup=$_GET['signup'];
+	if($check_signup=='empty')
+	{
+		print "<div class='error'><p>".'you must fill up the form to enter'.'</p></div>';
+	}
+	if($check_signup=='notregistered')
+	{
+		print "<div class='error'><p>".'wrong username or password'.'</p></div>';
+	}
+}
+	
+
+
+?>
+
+</body>
+</html>
